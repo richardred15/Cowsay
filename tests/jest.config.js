b5c66b65553420e50bfdd3e57789232c8263fbec
Richard R <richardred15@gmail.com>
2025-08-08 04:11:45 -0600

    Crawling with bugs

--- tests/jest.config.js
+++ tests/jest.config.js
@@ -1,16 +1,17 @@
 module.exports = {
-  testEnvironment: 'node',
-  roots: ['<rootDir>'],
-  testMatch: ['**/*.test.js'],
-  collectCoverageFrom: [
-    '../modules/**/*.js',
-    '../index.js',
-    '!../modules/games/**/*.js', // Game logic tested via integration
-    '!../node_modules/**'
-  ],
-  setupFilesAfterEnv: ['<rootDir>/fixtures/test-setup.js'],
-  testTimeout: 30000,
-  maxWorkers: 1, // Prevent database conflicts
-  verbose: true,
-  bail: true // Fail fast on security tests
+    testEnvironment: "node",
+    roots: ["<rootDir>"],
+    testMatch: ["**/*.test.js"],
+    collectCoverageFrom: [
+        "../modules/**/*.js",
+        "../index.js",
+        "!../modules/games/**/*.js", // Game logic tested via integration
+        "!../node_modules/**",
+    ],
+    setupFilesAfterEnv: ["<rootDir>/fixtures/test-setup.js"],
+    testTimeout: 30000,
+    maxWorkers: 1, // Prevent database conflicts
+    verbose: true,
+    bail: true, // Fail fast on security tests
+    forceExit: true,
 };
