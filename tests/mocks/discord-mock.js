0842e077c59effff35cd6ad0bcb31b2f70e4a497
Richard R <richardred15@gmail.com>
2025-08-10 12:28:40 -0600

    Added MessageFlags

--- tests/mocks/discord-mock.js
+++ tests/mocks/discord-mock.js
@@ -173,10 +173,27 @@ function createMockMessage(options = {}) {
     return message;
 }
 
+const MessageFlags = {
+    Crossposted: 1,
+    IsCrosspost: 2,
+    SuppressEmbeds: 4,
+    SourceMessageDeleted: 8,
+    Urgent: 16,
+    HasThread: 32,
+    Ephemeral: 64,
+    Loading: 128,
+    FailedToMentionSomeRolesInThread: 256,
+    SuppressNotifications: 4096,
+    IsVoiceMessage: 8192,
+    HasSnapshot: 16384,
+    IsComponentsV2: 32768,
+};
+
 // Export Discord.js-like structure
 module.exports = {
     createMockInteraction,
     createMockMessage,
+    MessageFlags,
     Client: MockClient,
     GatewayIntentBits: {
         Guilds: 1,
